a765617eb32f17e4b08f3c961c8d94b6e3d60c83
Chhote Pal (HCL TECHNOLOGIES CORPORATE SER) <v-chhotepal@microsoft.com>
2023-09-25 19:01:44 +0530

    initial

---
+++ src/test/java/com/spring/google/sso/SpringSSOGoogle/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.google.sso.SpringSSOGoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
